f9e8d7813a089fedc9f4f6d9e9a84a2e775130a6
TJ-Yusuke <jeeproud@gmail.com>
2021-12-08 22:20:43 +0900

    add: 動画編集機能をテスト実装

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+from moviepy.editor import *
+
+clip = VideoFileClip("./inputs/takeoff.mp4").subclip(0, 7)
+
+txt_clip = TextClip("takeoff", font="Amiri-regular", color="white", fontsize=100)
+txt_clip = txt_clip.set_position("center").set_duration(4)
+
+video = CompositeVideoClip([clip, txt_clip])
+
+video.write_videofile("./outputs/edited_takeoff4.mp4",
+                      codec="libx264", audio_codec="aac", temp_audiofile="temp-audio.m4a", remove_temp=True)
+
+
 def print_hi(name):
     print(f'Hi, {name}')
 
