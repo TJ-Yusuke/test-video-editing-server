c66238ee86414543e4e9e3ec69eb70ef9c197665
TJ-Yusuke <jeeproud@gmail.com>
2021-12-09 13:52:20 +0900

    add: firebase-storageに格納されている動画をダウンロードしてきて編集する機能をテスト実装

--- src/main.py
+++ src/main.py
@@ -1,22 +1,26 @@
 from moviepy.editor import *
+import urllib.request
 
-clip = VideoFileClip("./inputs/takeoff.mp4").subclip(0, 7)
+download_dst = "./inputs/downloaded_countdown.mp4"
+print("ダウンロード開始")
+
+urllib.request.urlretrieve(os.getenv("VIDEO_URL"), download_dst)
+
+print("ダウンロード完了")
+
+
+clip = VideoFileClip("./inputs/downloaded_countdown.mp4").subclip(0, 7)
 
 txt_clip = TextClip("テイクオフ！", font="Takao-Pゴシック", color="white", fontsize=120)
 txt_clip = txt_clip.set_position("center").set_duration(4)
 
 video = CompositeVideoClip([clip, txt_clip])
 
-video.write_videofile("./outputs/edited_takeoff.mp4",
+video.write_videofile("./outputs/downloaded_countdown2.mp4",
                       codec="libx264",
                       audio_codec="aac",
                       temp_audiofile="temp-audio.m4a",
                       remove_temp=True)
 
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+os.remove(download_dst)
+print("ダウンロードファイルを削除")
